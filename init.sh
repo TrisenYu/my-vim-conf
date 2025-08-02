87d535015ff8f00d432e8f09fa2d0221285c3d7a
trisenyu <kisfg@hotmail.com>
2025-08-03 01:25:08 +0800

    fix: unexpected sha256 representation{xxx...xxx -} leads to the bypass of vimrc at the end of `init.sh`. Delete `sort --version` in test-ci.yaml. Disable "auto-load vimrc" in vimrc.

--- init.sh
+++ init.sh
@@ -3,35 +3,16 @@
 # SPDX-LICENSE-IDENTIFIER: GPL2.0
 # (C) All rights reserved. Author: <kisfg@hotmail.com> in 2025
 # Created at 2025年07月06日 星期日 18时04分20秒
-# Last modified at 2025年08月03日 星期日 00时37分21秒
+# Last modified at 2025年08月03日 星期日 01时22分31秒
 # 我的评价是不如直接编程
 # TODO: 这么复杂的脚本居然没有getopts?
+#		感觉过于繁琐，还要在shellscript内验证参数
 set -u
 
-######################### 常量定义
-# github
-raw_github='https://raw.githubusercontent.com'
-main_github='https://github.com'
-res_mirror=''
-release_path='releases/download'
-url_prefix="$main_github"
-
-# 字体包名称
-mononame='JetBrainsMono-2.304'
-firaname='Fira_Code_v6.2'
-lxgwname='lxgw-wenkai-v1.520'
-# 这两个只有 zip
-mono_zip="$mononame.zip"
-fira_zip="$firaname.zip"
-lxgw_zip="$lxgwname.zip"
-# 有点难办
-sha256_list=(
-	'b20a708bfe76897bd4ad1e07521c657aca8f6ad0b07c5c13a9436969fb96a6ca'
-	'f80cbcaa8e827d2d0f693cc2188be746caa7d641ac7db6dece1cd49c1eec343a'
-	'f8c8e678ff3856de7bad2f37e896e0811fbc9b282bd74bae7b777226bf090170'
-)
+######################### 常/变量定义
 
 # 本地配置
+curr_dir=`pwd`
 vimdir="$HOME/.vim"
 fonts_dir="$HOME/.fonts/"
 color_path="$vimdir/colors"
@@ -39,8 +20,31 @@ init_dir="$vimdir/autoload"
 vim_target="$vimdir/vimrc"
 plugman="$init_dir/plug.vim"
 
-curr_dir=`pwd`
-vimrc_hash=`sha256sum "$curr_dir/vimrc"`
+# file-hash
+vimrc_hash=`sha256sum "$curr_dir/vimrc" | awk -F' ' '{ print $1 }'`
+# github
+raw_github='https://raw.githubusercontent.com'
+main_github='https://github.com'
+res_mirror=''
+release_path='releases/download'
+url_prefix="$main_github"
+vim_src_suf="vim/vim.git"
+
+# 字体包名称
+mononame='JetBrainsMono-2.304'
+firaname='Fira_Code_v6.2'
+lxgwname='lxgw-wenkai-v1.520'
+# TODO: 要不直接附在这个仓库作为assets算了
+mono_zip="$mononame.zip"
+fira_zip="$firaname.zip"
+lxgw_zip="$lxgwname.zip"
+sha256_list=(
+	'b20a708bfe76897bd4ad1e07521c657aca8f6ad0b07c5c13a9436969fb96a6ca'
+	'f80cbcaa8e827d2d0f693cc2188be746caa7d641ac7db6dece1cd49c1eec343a'
+	'f8c8e678ff3856de7bad2f37e896e0811fbc9b282bd74bae7b777226bf090170'
+)
+
+# git-commit-hash
 color_schash='040138616bec342d5ea94d4db296f8ddca17007a'
 plugman_hash='baa66bcf349a6f6c125b0b2b63c112662b0669e1'
 
@@ -52,7 +56,13 @@ famous_mirrors=(
 	'gh.llkk.cc'
 	'ghproxy.net'
 	'ghproxy.homeboyc.cn'
+	# https://cdn.jsdelivr.net/gh/ github-repository-suffix
 )
+
+# TODO: 搞成配置
+ping_times=3
+inf_val=31415926535897932384626433
+
 
 ########################################################## 函数定义
 function help() {
@@ -61,42 +71,40 @@ function help() {
 END_OF_LINE
 }
 
+
 # ping 所有给定的镜像站，取时长最小、丢包数最小的一个
 function _probe() {
 	mirr_str=""
 	for cur_mirr in ${famous_mirrors[@]}; do
-		# TODO: ping的次数搞成配置 ping也并发
-		mid_rtt_val=`ping -c 2 $cur_mirr`
+		# TODO: ping也并发
+		mid_rtt_val=`ping -c $ping_times $cur_mirr`
 		loss_rate=`echo "$mid_rtt_val" | grep -oP '([0-9\.]+)(?=% packet loss)'`
 		rtt_val=`\
 			echo "$mid_rtt_val" | grep 'rtt min/avg/max/mdev = [0-9\./]\+ ms$' | \
 			awk -F'/' '{ print $6 }'
 		`
-		# issue1: ping包比较小就不会携带rtt信息
-		#	例如 2 packets transmited, 0 received, 100% packet loss , time 1022ms
-		# issue2: ping失败后返回1触发set -e的满足条件，导致整个shellscript挂了
 		if [[ "$rtt_val" == "" ]]; then
 			rtt_val=`echo "$mid_rtt_val" | grep -o ' [0-9\.]\+ms$' | awk -F'ms' '{ print $1 }'`
-			rtt_val=`echo "scale=6;$rtt_val/2" | bc`
+			rtt_val=`echo "scale=6;$rtt_val/$ping_times" | bc`
 		fi
 		# 否则认为站点不可达
-		[[ "$rtt_val" == "" ]] && rtt_val=31415926535897932384626433
+		[[ "$rtt_val" == "" ]] && rtt_val=$inf_val
 		mirr_str+="$cur_mirr,$rtt_val,$loss_rate\n"
 	done
 	# 策略:
 	#	时间小的优先
 	# 	时间一致的前提下选丢包率靠近零的
-	mirror_tbl=(`echo -e "${mirr_str:0:-2}" | sort -n -t ' ' -k2 -k3 | tr -n "\n" ' '`)
+	mirror_tbl=(`echo -e "${mirr_str:0:-2}" | sort -n -t ' ' -k2 -k3 | tr "\n" ' '`)
 	mirr_check=`echo -e "$mirror_tbl" | awk -F' ' '{ print $3 }' | head -n 1`
-	choice_rec=31415926535897932384626433
+	choice_val=$inf_val
 	for item in ${mirror_tbl[@]}; do
 		curr_mirr=(`echo "$item" | tr ',' ' '`)
 		mirr_check=${curr_mirr[2]}
 		mirr_speed=${curr_mirr[1]}
 		[[ "$mirr_check" -ge 90 ]] && continue
-		if [[ "$choice_rec" -gt "$mirr_speed" ]]; then
-			choice_rec="$mirr_speed"
-			res_mirror=${curr_mirr[0]}
+		if [[ "$choice_val" -gt "$mirr_speed" ]]; then
+			choice_val="$mirr_speed"
+			res_mirror="${curr_mirr[0]}"
 		fi
 	done
 	unset rtt_dict mirr_str mirror_tbl mirr_check choice_rec
@@ -122,6 +130,7 @@ function alter_src_via_mirror() {
 	echo "choice: $res_mirror" "$url_prefix" "$raw_github"
 }
 
+
 # 入参:  压缩包名称 url
 function _detect_font() {
 	mkdir -p "$fonts_dir" && cd "$fonts_dir"
@@ -139,9 +148,9 @@ function _detect_font() {
 		if [[ -d "$payload" ]]; then
 			# 只要字典序的哈希结果，这个 -df 太阴了
 			ret=`\
-				find "$payload" -type f | sort -df | xargs sha256sum | \
-				awk -F' ' '{ printf $1"\n" }' | sha256sum | \
-				awk -F' ' '{ print $1 }' \
+				find "$payload" -type f | sort -df | \
+				xargs sha256sum | awk -F' ' '{ printf $1"\n" }' | \
+				sha256sum | awk -F' ' '{ print $1 }' \
 			`
 			# 文件有而且齐全
 			[[ "$ret" == "${sha256_list[i]}" ]] && continue
@@ -158,6 +167,7 @@ function _detect_font() {
 	wait
 	cd "$curr_dir"
 }
+
 
 function get_fonts() {
 	# 直接固定写死用这些版本的字体
@@ -180,6 +190,8 @@ function get_color_scheme() {
 	# 加入判断，避免重复下载
 	[[ -d "$color_path" && -f "$color_path/gruvbox.vim" ]] && return
 	{
+		# TODO: cdn.jsdelivr.net/gh 下
+		# raw-github的形式：https://cdn.jsdelivr.net/gh/morhetz/gruvbox@master/colors/gruvbox.vim
 		wget -P "$color_path" \
 			"$raw_github/morhetz/gruvbox/$color_schash/colors/gruvbox.vim" &> /dev/null
 	}&
@@ -240,6 +252,18 @@ $payload
 	echo "$pre_process" > "$fontconf"
 }
 
+
+function _cp_vimrc() {
+	cp "$curr_dir/vimrc" "$vimdir/vimrc"
+	vim -u "$vimdir/vimrc" +PlugInstall! +wa! &> /dev/null
+}
+
+
+function _clone_vim_src() {
+	# TODO: 到哪个目录？
+	#		要不要直接在init.sh调用这个下载安装脚本？不然量太大了
+	git clone --depth=2 --recursive "$url_prefix"
+}
 ####################################################################### shellscript入口
 # 准备创建管道文件
 # trap oops SIGINT SIGILL
@@ -253,11 +277,17 @@ $payload
 
 ## TODO: 确定vim版本而决定是否需要从头开始编译，并编写可用的编译脚本
 # vim --version
+# arch
+#	sudo pacman -Rsn vi vim-tiny vim vim-runtime gvim vim-common vim-gui-common vim-nox
+# debian
+#	sudo apt-get remove --purge vi vim-tiny vim vim-runtime gvim vim-common vim-gui-common vim-nox
 
-if [[ ! -s "$vim_target" || `cat "$vim_target" | sha256sum` != "$vimrc_hash" ]]; then
+if [[ -s "$vim_target" ]]; then
 	# 经过color后已经创建~/.vim/目录
-	cp "$curr_dir/vimrc" "$vimdir/vimrc"
-	vim -u "$vimdir/vimrc" +PlugInstall! +wa! &> /dev/null
+	calc_hash=`cat "$vim_target"|sha256sum|awk -F' ' '{print $1}'`
+	[[ "$calc_hash" != "$vimrc_hash" ]] && "_cp_vimrc"
+else
+	"_cp_vimrc"
 fi
 echo 'done...'
 
